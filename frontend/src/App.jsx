d45402c7fd91e3d0eb691804fa5eed08d0ed1a65
Dikshant <khuranadikshant3@gmail.com>
2024-03-02 22:24:46 +0530

    routing updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,22 @@ import Venues from "./components/pages/Venues";
 import Navbar from "./components/navbar/navbar";
 import Background from "./components/background/Background";
 import Teacher from "./components/pages/Teacher";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
         <>
-            <Navbar />
-            <Background />
-            {/* <Home /> */}
-            {/* <Venues /> */}
-            <Teacher />
+            <Router>
+                <Navbar />
+                <Background />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/venues" element={<Venues />} />
+                    <Route exact path="/teachers" element={<Teacher />}></Route>
+                </Routes>
+            </Router>
         </>
     );
 }
