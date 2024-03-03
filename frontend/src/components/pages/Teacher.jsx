4473220649fe4ac881e6ac9b56e68faf85a5d9ec
Dikshant <khuranadikshant3@gmail.com>
2024-03-03 06:10:21 +0530

    teacher updated

--- frontend/src/components/pages/Teacher.jsx
+++ frontend/src/components/pages/Teacher.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Teacher.css";
 
 const Teacher = () => {
+    const [formSubmitted, setFormSubmitted] = useState(false);
+
     const formStyle = {
         position: "absolute",
-        top: "50%",
+        top: formSubmitted ? "20%" : "50%",
         width: "40%",
         left: "50%",
         transform: "translate(-50%, -50%)",
     };
 
-    // const btnStyle = {
-    //     backgroundColor: "#009381",
-    //     color: "#fff",
-    //     cursor: "pointer", // Add a pointer cursor on hover
-    //     transition: "background-color 0.3s ease, color 0.3s ease",
-    // };
+    const cardStyle = {
+        width: "18rem",
+        height: "18rem",
+        "margin-top": "20px",
+        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+        transition: "transform 0.3s ease-in-out",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setFormSubmitted(true);
+    };
 
     return (
         <div>
-            <form className="form-inline d-flex" style={formStyle}>
+            <form
+                className="form-inline d-flex"
+                style={formStyle}
+                onSubmit={handleSubmit}
+            >
                 <input
                     class="form-control mr-sm-2"
                     type="search"
@@ -33,6 +49,19 @@ const Teacher = () => {
                     Search
                 </button>
             </form>
+            {formSubmitted && (
+                <div className="card" style={cardStyle}>
+                    <img
+                        src="https://images.pexels.com/photos/2280571/pexels-photo-2280571.jpeg?auto=compress&cs=tinysrgb&w=600"
+                        className="card-img-top"
+                        alt="..."
+                        style={{ height: "18rem" }}
+                    />
+                    <div className="card-body">
+                        <p className="card-text">Labs</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
